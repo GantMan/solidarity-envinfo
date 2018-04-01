f4609ff6908effcc18ab3326b9e3c427bf6092bc
Gant <GantMan@gmail.com>
2018-04-01 00:29:42 -0500

    infoReport rule works

--- extensions/envinfo.js
+++ extensions/envinfo.js
@@ -1,6 +1,3 @@
-const addOptionalRules = require('./helpers/addOptionalRules')
-const getAndroidEnvData = require('./helpers/getAndroidEnvData')
-
 module.exports = (context) => {
   // Register this plugin
   context.addPlugin({
@@ -9,24 +6,39 @@ module.exports = (context) => {
     rules: {
       infoReport: {
         report: async (rule, context, report) => {
-          const { print } = context
-          const { colors } = print
+          const envinfo = require('envinfo')
+          const { toPairs, map } = require('ramda')
 
-          // const projectAPIMessage = availableApiVersions.includes(projectApiVersion)
-          //   ? colors.green(`API ${projectApiVersion} Available`)
-          //   : colors.red(`API ${projectApiVersion} Unavailable`)
+          const envResults = toPairs(JSON.parse(envinfo.run(rule.report, { json: true })))
 
-          // const projectBuildToolsMessage = availableBuildToolsVersions.includes(projectBuildToolsVersion)
-          //   ? colors.green(`Build Tools ${projectBuildToolsVersion} Available`)
-          //   : colors.red(`Build Tools ${projectBuildToolsVersion} Unavailable`)
+          const envTables = map(envItem => {
+            const [topLevel, results] = envItem
+            const allResults = toPairs(results)
+            if (topLevel === 'npmPackages') {
+              const subResults = toPairs(allResults)
+              const tableizedNpm = map(fun => {
+                const eachPackage = fun[1]
+                return [eachPackage[0], eachPackage[1]['wanted'], eachPackage[1]['installed']]
+              }, subResults)
+              return {
+                title: 'Reported NPM Packages',
+                table: [
+                  ['Package', 'Wanted', 'Installed'],
+                  ...tableizedNpm
+                ]
+              }
+            } else {
+              return {
+                title: topLevel,
+                table: [
+                  ['key', 'value'],
+                  ...allResults
+                ]
+              }
+            }
+          }, envResults)
 
-          report.customRules.push({
-            title: 'Requested Environment Info',
-            table: [
-              ['Key', 'Value'],
-              [projectAPIMessage, projectBuildToolsMessage],
-            ]
-          })
+          report.customRules = [...envTables, ...report.customRules]
         }
       }
     }
