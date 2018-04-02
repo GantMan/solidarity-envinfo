d75d1f7dacec350ebf4d8fbd846df983f30f9d39
Gant Laborde <GantMan@gmail.com>
2018-04-02 17:32:07 -0500

    enforcement working

--- extensions/helpers/checkEnv.js
+++ extensions/helpers/checkEnv.js
@@ -1,8 +1,30 @@
 module.exports = async (rule, context) => {
   const envinfo = require('envinfo')
+  const { values, map, toPairs, contains, reject, isEmpty } = require('ramda')
 
-  return {
-    pass: true,
-    message: `Environment Info Good MOFUCKA`
+  const envResults = toPairs(JSON.parse(envinfo.run(rule.report, { json: true })))
+
+  const failures = map(envItem => {
+    const [topLevel, results] = envItem
+
+    if (contains('Not Found', values(results))) {
+      return `Env Fail for ${topLevel} -> ${toPairs(results)}`
+    } else {
+      return ''
+    }
+  }, envResults)
+
+  const errors = reject(isEmpty, failures)
+
+  if (errors.length > 0) {
+    return {
+      pass: false,
+      message: errors.join(', ')
+    }
+  } else {
+    return {
+      pass: true,
+      message: `Enforced envinfo all good`
+    }
   }
 }
